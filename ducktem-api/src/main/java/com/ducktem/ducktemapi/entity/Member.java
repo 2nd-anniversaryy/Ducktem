583f92e180a408baa9733dea22992c42530b79eb
hantaehee <xogmlwkdi@naver.com>
2023-01-18 16:49:08 +0900

    [#3] 멤버 어노테이션 수정 @AllArgsConstructor,@Builder

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/entity/Member.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/entity/Member.java
@@ -12,12 +12,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Setter
 @ToString
