b6fe4864eacfb39a1e3ea8a34454a52e899e2cbf
hantaehee <xogmlwkdi@naver.com>
2023-01-20 17:49:25 +0900

    [#13] 유저 아이디로 멤버찾는 query Method 추가

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/repository/MemberRepository.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/repository/MemberRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ducktem.ducktemapi.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member,Integer> {
+public interface MemberRepository extends JpaRepository<Member,Long> {
+	Member findByUserId(String userId);
+
+
 }
