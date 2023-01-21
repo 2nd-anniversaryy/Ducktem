8a603dbdbf21c2d55b67955da0d8231ad367f1fe
hantaehee <xogmlwkdi@naver.com>
2023-01-21 23:25:58 +0900

    [#13] feat : 회원가입 시 회원 중복 확인 구현 회원 중복 확인을 위해 userId, nickName으로 회원 조회 후 없다면 저장하고 있다면 예외를 발생시켜 처리하였음.

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/service/MemberServiceImpl.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/service/MemberServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ducktem.ducktemapi.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.ducktem.ducktemapi.dto.MemberDto;
 import com.ducktem.ducktemapi.entity.Member;
-import com.ducktem.ducktemapi.entity.MemberDto;
+import com.ducktem.ducktemapi.exception.MemberException;
 import com.ducktem.ducktemapi.jwt.JwtProvider;
 import com.ducktem.ducktemapi.repository.MemberRepository;
 
@@ -20,19 +22,43 @@ public class MemberServiceImpl implements MemberService {
 
 	@Override
 	public Member get(String userId) {
-		return memberRepository.findByUserId(userId);
+
+		Optional<Member> member = memberRepository.findByUserId(userId);
+		if (member.isEmpty()) {
+			throw new MemberException("존재하지 않는 회원입니다.");
+		}
+
+		return member.get();
 	}
 
 	@Override
 	public Member join(Member member) {
-
+		duplicateCheck(member);
 		return memberRepository.save(member);
+	}
+
+	// 회원가입시 id,nickname을 확인 한 후 기존 아이디가 존재한다면 예외를 발생시킴
+	public void duplicateCheck(Member member) {
+		Optional<Member> userId = memberRepository.findByUserId(member.getUserId());
+		Optional<Member> nickName = memberRepository.findByNickName(member.getNickName());
+
+		if (userId.isPresent()) {
+			throw new MemberException("아이디가 중복되었습니다.");
+		} else if (nickName.isPresent()) {
+			throw new MemberException("닉네임이 중복되었습니다.");
+		}
 	}
 
 	@Override
 	public String login(MemberDto memberDto) {
-		String userId = memberDto.getUserId();
-		return jwtProvider.createJwt(userId);
+		String userId = null;
+
+		if (isValid(memberDto)) {
+			userId = memberDto.getUserId();
+			return jwtProvider.createJwt(userId);
+		}
+
+		return "회원이 아닙니다.";
 	}
 
 	@Override
@@ -40,4 +66,8 @@ public class MemberServiceImpl implements MemberService {
 		return null;
 	}
 
+	private boolean isValid(MemberDto memberDto) {
+		Optional<Member> member = memberRepository.findByUserIdAndPwd(memberDto.getUserId(), memberDto.getPwd());
+		return member.isPresent();
+	}
 }
