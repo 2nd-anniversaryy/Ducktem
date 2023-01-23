42647365f5ab964831d0af81be83610ec45683b5
hantaehee <xogmlwkdi@naver.com>
2023-01-24 00:19:19 +0900

    [#13] feat : 회원 기능 추가구현 비밀번호를 bcrypt로 인코딩하여 저장으로 변경, 리프레쉬 토큰을 같이 발급, 로그인과 회원가입 예외처리 추가.

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/service/MemberServiceImpl.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/service/MemberServiceImpl.java
@@ -1,15 +1,21 @@
 package com.ducktem.ducktemapi.service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ducktem.ducktemapi.dto.MemberDto;
 import com.ducktem.ducktemapi.entity.Member;
+import com.ducktem.ducktemapi.entity.MemberStatus;
 import com.ducktem.ducktemapi.exception.MemberException;
 import com.ducktem.ducktemapi.jwt.JwtProvider;
 import com.ducktem.ducktemapi.repository.MemberRepository;
+import com.ducktem.ducktemapi.repository.RefreshTokenRepository;
+import com.ducktem.ducktemapi.util.TimeFormatter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,7 +24,9 @@ import lombok.RequiredArgsConstructor;
 public class MemberServiceImpl implements MemberService {
 	private final MemberRepository memberRepository;
 
+	private final PasswordEncoder passwordEncoder;
 	private final JwtProvider jwtProvider;
+	private final RefreshTokenRepository refreshTokenRepository;
 
 	@Override
 	public Member get(String userId) {
@@ -31,10 +39,41 @@ public class MemberServiceImpl implements MemberService {
 		return member.get();
 	}
 
+	@Transactional
 	@Override
 	public Member join(Member member) {
 		duplicateCheck(member);
+		member.setStatus(MemberStatus.ENABLE);
+		member.setPwd(passwordEncoder.encode(member.getPwd()));
+		member.setRegDate(TimeFormatter.NOW());
 		return memberRepository.save(member);
+	}
+
+	@Override
+	@Transactional
+	public Map<String, String> login(MemberDto memberDto) {
+		Member member = valid(memberDto);
+		String refreshJwt = jwtProvider.createRefreshJwt();
+		String accessJwt = jwtProvider.createAccessJwt(member.getUserId());
+
+		return Map.of("refreshJwt", refreshJwt, "accessJwt", accessJwt);
+	}
+
+	@Override
+	public List<Member> getList() {
+		return null;
+	}
+
+	// 아이디,패스워드 별로 예외를 나눔.
+	private Member valid(MemberDto memberDto) {
+		Optional<Member> member = memberRepository.findByUserId(memberDto.getUserId());
+		if (member.isPresent()) {
+			if (!passwordEncoder.matches(memberDto.getPwd(), member.get().getPwd()))
+				throw new MemberException("비밀번호가 다릅니다.");
+		} else {
+			throw new MemberException("회원아이디가 존재하지 않습니다.");
+		}
+		return member.get();
 	}
 
 	// 회원가입시 id,nickname을 확인 한 후 기존 아이디가 존재한다면 예외를 발생시킴
@@ -47,27 +86,5 @@ public class MemberServiceImpl implements MemberService {
 		} else if (nickName.isPresent()) {
 			throw new MemberException("닉네임이 중복되었습니다.");
 		}
-	}
-
-	@Override
-	public String login(MemberDto memberDto) {
-		String userId = null;
-
-		if (isValid(memberDto)) {
-			userId = memberDto.getUserId();
-			return jwtProvider.createJwt(userId);
-		}
-
-		return "회원이 아닙니다.";
-	}
-
-	@Override
-	public List<Member> getList() {
-		return null;
-	}
-
-	private boolean isValid(MemberDto memberDto) {
-		Optional<Member> member = memberRepository.findByUserIdAndPwd(memberDto.getUserId(), memberDto.getPwd());
-		return member.isPresent();
 	}
 }
