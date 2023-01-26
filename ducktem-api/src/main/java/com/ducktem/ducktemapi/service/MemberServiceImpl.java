847859ac128e9392ea75e3329f4a2bab11c7a355
hantaehee <xogmlwkdi@naver.com>
2023-01-26 22:03:32 +0900

    [#13] Refactor : 로그인 반환 타입 변경 tokenResponse로 반환하는걸로 변경

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/service/MemberServiceImpl.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/service/MemberServiceImpl.java
@@ -1,7 +1,6 @@
 package com.ducktem.ducktemapi.service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ducktem.ducktemapi.dto.MemberDto;
+import com.ducktem.ducktemapi.dto.response.TokenResponse;
 import com.ducktem.ducktemapi.entity.Member;
 import com.ducktem.ducktemapi.entity.MemberStatus;
 import com.ducktem.ducktemapi.entity.RefreshToken;
@@ -52,19 +52,18 @@ public class MemberServiceImpl implements MemberService {
 
 	@Override
 	@Transactional
-	public Map<String, String> login(MemberDto memberDto) {
+	public TokenResponse login(MemberDto memberDto) {
 		Member member = valid(memberDto);
 		String refreshJwt = jwtProvider.createRefreshJwt();
 		String accessJwt = jwtProvider.createAccessJwt(member.getUserId());
 		Optional<RefreshToken> existingMember = refreshTokenRepository.findByMember(member);
-		System.out.println(existingMember);
 		if (existingMember.isEmpty()) {
 			refreshTokenRepository.save(RefreshToken.builder().refreshToken(refreshJwt).member(member).build());
 		} else {
 			existingMember.get().setRefreshToken(refreshJwt);
 		}
 
-		return Map.of("refreshJwt", refreshJwt, "accessJwt", accessJwt);
+		return TokenResponse.from(accessJwt, refreshJwt);
 	}
 
 	@Override
