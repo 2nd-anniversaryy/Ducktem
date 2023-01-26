72307e3f98f12a27ae6c550542be6b1567b4523b
hantaehee <xogmlwkdi@naver.com>
2023-01-26 23:54:27 +0900

    [#22] refactor : setter 제거 setter 사용 지양.

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/entity/ProductImage.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/entity/ProductImage.java
@@ -12,13 +12,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Setter
 @Getter
 @ToString
 @Entity
