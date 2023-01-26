9cafd5ce5a3fdebe07c22b1a2f8ce3281b316cf8
hantaehee <xogmlwkdi@naver.com>
2023-01-26 19:54:43 +0900

    [#22] feat : 상품 조회 페이징 추가

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/repository/ProductRepository.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.ducktem.ducktemapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ducktem.ducktemapi.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-
+	Page<Product> findAll(Pageable pageable);
 }
