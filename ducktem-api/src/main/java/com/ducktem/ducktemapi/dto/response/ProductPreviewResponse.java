e4fd3ee01fa044b91ba2e1d105089934ec740416
miji7575 <mipark7575@gmail.com>
2023-01-27 21:10:43 +0900

    fix: orElseGet 로 썸네일 이미지가 없는 경우 처리

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/dto/response/ProductPreviewResponse.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/dto/response/ProductPreviewResponse.java
@@ -1,6 +1,9 @@
 package com.ducktem.ducktemapi.dto.response;
 
+import com.ducktem.ducktemapi.entity.Category;
 import com.ducktem.ducktemapi.entity.Product;
+import com.ducktem.ducktemapi.entity.ProductImage;
+import com.ducktem.ducktemapi.entity.SalesStatus;
 import com.ducktem.ducktemapi.exception.ProductException;
 
 import lombok.AllArgsConstructor;
@@ -17,8 +20,11 @@ public class ProductPreviewResponse {
 	private String name;
 	private String price;
 	private String regDate;
-	private String salesStatus;
+	private SalesStatus salesStatus;
 	private String thumbNail;
+
+
+
 
 
 	// 상품 정보에서 필요한 데이터만 취합하여 생성.
@@ -34,8 +40,10 @@ public class ProductPreviewResponse {
 				.stream()
 				.filter(image -> image.getThumbNail() == (byte)1)
 				.findFirst()
-				.orElseThrow(() -> new ProductException("데이터 오륲"))
+				.orElseGet(()->new ProductImage())
+				// .orElseThrow(() -> new ProductException("데이터 오륲"))
 				.getName())
+			.salesStatus(product.getSalesStatus())
 			.build();
 	}
 
