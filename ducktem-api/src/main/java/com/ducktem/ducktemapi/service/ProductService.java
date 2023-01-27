5716dcbfd078f646e722b1758c29ed5c93216460
miji7575 <mipark7575@gmail.com>
2023-01-27 21:16:38 +0900

    feat : 상품제목을 검색하여 상품목록 불러오기, 카테고리로 상품목록 불러오기 구현

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/service/ProductService.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 
 import com.ducktem.ducktemapi.dto.response.ProductDetailResponse;
 import com.ducktem.ducktemapi.dto.response.ProductPreviewResponse;
+import com.ducktem.ducktemapi.entity.Category;
 import com.ducktem.ducktemapi.entity.Product;
 
 public interface ProductService {
@@ -15,4 +16,10 @@ public interface ProductService {
 	Product add(Product product, String regMemberId);
 
 	List<ProductPreviewResponse> getList(Pageable pageable);
+
+	List<ProductPreviewResponse> getListByQuery(Pageable pageable, String query);
+
+	List<ProductPreviewResponse> getListByCategory(Pageable pageable, int category);
+
+
 }
