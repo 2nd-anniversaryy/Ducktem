7a9d37cce1f2bde55c9913ed89005ae367812241
hantaehee <xogmlwkdi@naver.com>
2023-01-30 20:14:20 +0900

    [#22] feat : 상품 이미지 저장 메소드 추가 상품 url 경로를 얻어온 후 url을 디비에 저장

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/service/ProductImageServiceImpl.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/service/ProductImageServiceImpl.java
@@ -4,15 +4,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.ducktem.ducktemapi.entity.Product;
 import com.ducktem.ducktemapi.entity.ProductImage;
 import com.ducktem.ducktemapi.repository.ProductImageRepository;
+import com.ducktem.ducktemapi.util.ImageUtil;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
-public class ProductImageServiceImpl implements ProductImageService{
+public class ProductImageServiceImpl implements ProductImageService {
 
 	private final ProductImageRepository imageRepository;
 
@@ -20,9 +24,8 @@ public class ProductImageServiceImpl implements ProductImageService{
 	public List<ProductImage> getThumbNailList() {
 		Optional<List<ProductImage>> thumbNails = imageRepository.findByThumbNail((byte)1);
 		List<ProductImage> thumbNailList = null;
-		if(thumbNails.isPresent())
+		if (thumbNails.isPresent())
 			thumbNailList = thumbNails.get();
-
 
 		return thumbNailList;
 	}
@@ -30,5 +33,25 @@ public class ProductImageServiceImpl implements ProductImageService{
 	@Override
 	public List<ProductImage> getImages() {
 		return null;
+	}
+
+	@Override
+	@Transactional
+	public void add(MultipartFile[] files, Product product) {
+		int i = 0;
+		System.out.println(files.length);
+		for (MultipartFile file : files) {
+			String imgUrl = ImageUtil.save(file);
+			if (imgUrl != null) {
+				if (i == 0) {
+					imageRepository.save(
+						ProductImage.builder().name(imgUrl).product(product).thumbNail((byte)1).build());
+				} else {
+					imageRepository.save(
+						ProductImage.builder().name(file.getName()).product(product).thumbNail((byte)0).build());
+				}
+				i++;
+			}
+		}
 	}
 }
