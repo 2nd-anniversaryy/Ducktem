1574a21a18c17972cee53387c11ffe4019828273
hantaehee <xogmlwkdi@naver.com>
2023-01-30 20:35:16 +0900

    docs : 파일 포멧터 적용 및 불필요 import 제거

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/service/ProductService.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/service/ProductService.java
@@ -1,18 +1,12 @@
 package com.ducktem.ducktemapi.service;
 
-
+import com.ducktem.ducktemapi.dto.request.ProductRegisterRequest;
 import com.ducktem.ducktemapi.dto.response.ProductDetailResponse;
-import com.ducktem.ducktemapi.dto.response.ProductPreviewResponse;
-import com.ducktem.ducktemapi.entity.Category;
 import com.ducktem.ducktemapi.entity.Product;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 public interface ProductService {
 	ProductDetailResponse get(Long id);
-	Product add(Product product, String regMemberId);
 
+	Product add(ProductRegisterRequest request, String regMemberId);
 
 }
