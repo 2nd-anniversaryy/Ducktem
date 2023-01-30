d7938b9213f43a6fdd63fcab2067e7e05debbfbb
miji7575 <mipark7575@gmail.com>
2023-01-30 16:32:22 +0900

    docs : getList기능을 가진 함수 통합

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/controller/ProductController.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/controller/ProductController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.ducktem.ducktemapi.dto.response.ProductPreviewResponse;
 import com.ducktem.ducktemapi.entity.Category;
 import com.ducktem.ducktemapi.entity.Product;
 import com.ducktem.ducktemapi.service.ProductService;
+import com.ducktem.ducktemapi.service.SearchService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,80 +31,62 @@ import lombok.RequiredArgsConstructor;
 public class ProductController {
 
 	private final ProductService productservice;
-	// /products?p=1&s=15
+	private final SearchService searchService;
+
+
+
 	@GetMapping
-	public List<ProductPreviewResponse> getList(@PageableDefault(size = 20) Pageable pageable)
-	{
-		return productservice.getList(pageable);
+	@Transactional
+	public Map<String,Object> getList(
+		@PageableDefault(size = 20) Pageable pageable,
+		@RequestParam(name = "q", defaultValue = "") String query,
+		@RequestParam(name = "c", defaultValue = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15")Integer[] category,
+		@RequestParam(name="f", defaultValue = "최신순") String filter
+	){
+		Map<String ,Object> searchResultByCategory =new HashMap<>();
+
+		Long countResult = searchService.getCountByQuery(query,category);
+		searchResultByCategory.put("countResult",countResult);
+
+		switch (filter){
+			case "최신순":
+				List<ProductPreviewResponse> productResult= searchService.getListByCategoryAndSearch(pageable, query, category);
+				searchResultByCategory.put("productResult",productResult);
+				break;
+			case "높은가격순":
+				List<ProductPreviewResponse> productResultByPriceDesc= searchService.getListByCategoryAndSearchOrderByPriceDesc(pageable, query, category);
+				searchResultByCategory.put("productResult",productResultByPriceDesc);
+				break;
+			case "낮은가격순":
+				List<ProductPreviewResponse> productResultByPrice= searchService.getListByCategoryAndSearchOrderByPrice(pageable, query, category);
+				searchResultByCategory.put("productResult",productResultByPrice);
+				break;
+			case "인기상품순":
+				List<ProductPreviewResponse> productResultByHit= searchService.getListByCategoryAndSearchOrderByHit(pageable, query, category);
+				searchResultByCategory.put("productResult",productResultByHit);
+				break;
+		}
+
+		return searchResultByCategory;
 	}
 
-	// /products/2
+
+
+
+	// 상품 상세 페이지 요청 /products/2
 	@GetMapping("{id}")
 	public ProductDetailResponse get(@PathVariable Long id) {
 		return productservice.get(id);
 	}
 
+
+
+
+	//상품 추가
 	@PostMapping
 	public Product add(@RequestBody Product product, Authentication authentication) {
 		String regMemberId = authentication.getName();
 		return productservice.add(product, regMemberId);
-	}
-
-	@GetMapping("/category")
-	public List<ProductPreviewResponse> getListByCategory(
-												@PageableDefault(size = 200) Pageable pageable,
-												@RequestParam(name = "c", defaultValue = "")Integer[] category
-												){
-		return productservice.getListByCategory(pageable, category);
-	}
-
-	@GetMapping("/searchByCategory")
-	public Map<String,Object> getListByCategoryAndSearch(
-		@PageableDefault(size = 20) Pageable pageable,
-		@RequestParam(name = "q", defaultValue = "") String query,
-		@RequestParam(name = "c", defaultValue = "")Integer[] category,
-		@RequestParam(name="f", defaultValue = "최신순") String filter
-	){
-		Map<String ,Object> searchResultByCategory =new HashMap<>();
-
-		Long countResult = productservice.getCountByQuery(query,category);
-		searchResultByCategory.put("countResult",countResult);
-		System.out.println(filter);
-		switch (filter){
-			case "최신순":
-				List<ProductPreviewResponse> productResult= productservice.getListByCategoryAndSearch(pageable, query, category);
-				searchResultByCategory.put("productResult",productResult);
-				break;
-			case "높은가격순":
-				List<ProductPreviewResponse> productResultByPriceDesc= productservice.getListByCategoryAndSearchOrderByPriceDesc(pageable, query, category);
-				searchResultByCategory.put("productResult",productResultByPriceDesc);
-				System.out.println(filter);
-				break;
-			case "낮은가격순":
-				List<ProductPreviewResponse> productResultByPrice= productservice.getListByCategoryAndSearchOrderByPrice(pageable, query, category);
-				searchResultByCategory.put("productResult",productResultByPrice);
-				System.out.println(filter);
-				break;
-		}
-
-
-
-		return searchResultByCategory;
-	}
-
-	@GetMapping("/search")
-	public Map<String,Object> getListBySearch(
-		@PageableDefault(size = 20) Pageable pageable,
-		@RequestParam(name = "q", defaultValue = "") String query,
-		@RequestParam(name = "c", defaultValue = "")Integer[] category
-	){
-		Map<String,Object> searchResult = new HashMap<>();
-		List<ProductPreviewResponse> productResult = productservice.getListBySearch(pageable, query);
-		Long countResult = productservice.getCountByQuery(query,category);
-		searchResult.put("productResult",productResult);
-		searchResult.put("countResult",countResult);
-
-		return searchResult;
 	}
 
 
