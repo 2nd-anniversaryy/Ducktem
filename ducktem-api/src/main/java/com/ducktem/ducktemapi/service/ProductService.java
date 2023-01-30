696f6131569be9a0103d7d6ec53327d97e2ecd6f
hantaehee <xogmlwkdi@naver.com>
2023-01-30 20:17:01 +0900

    [#22] feat : 상품 등록 메소드 추가 구현 상품 엔티티를 바로 받는것이 아닌 요청정보를 받는것으로 변경

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/service/ProductService.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/service/ProductService.java
@@ -1,19 +1,18 @@
 package com.ducktem.ducktemapi.service;
 
-
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 
+import com.ducktem.ducktemapi.dto.request.ProductRegisterRequest;
 import com.ducktem.ducktemapi.dto.response.ProductDetailResponse;
 import com.ducktem.ducktemapi.dto.response.ProductPreviewResponse;
-import com.ducktem.ducktemapi.entity.Category;
 import com.ducktem.ducktemapi.entity.Product;
 
 public interface ProductService {
 	ProductDetailResponse get(Long id);
 
-	Product add(Product product, String regMemberId);
+	Product add(ProductRegisterRequest request, String regMemberId);
 
 	List<ProductPreviewResponse> getList(Pageable pageable);
 
@@ -23,7 +22,6 @@ public interface ProductService {
 
 	List<ProductPreviewResponse> getListByCategory(Pageable pageable, Integer[] category);
 
-	List<ProductPreviewResponse> getListByCategoryAndSearch(Pageable pageable,String query,Integer[] category);
-
+	List<ProductPreviewResponse> getListByCategoryAndSearch(Pageable pageable, String query, Integer[] category);
 
 }
