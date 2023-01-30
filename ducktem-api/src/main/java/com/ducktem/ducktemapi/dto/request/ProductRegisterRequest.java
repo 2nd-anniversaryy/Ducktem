b760957596f3e2ec0d5bcc8589d7d621870b0338
hantaehee <xogmlwkdi@naver.com>
2023-01-30 20:19:49 +0900

    [#22] feat : 상품 등록 요청 클래스 생성 상품 등록 시 요청하는 데이터만 받아오는 클래스

---
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/dto/request/ProductRegisterRequest.java
@@ -0,0 +1,24 @@
+package com.ducktem.ducktemapi.dto.request;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRegisterRequest {
+	private String name;
+	private String price;
+	private String description;
+	private String condition;
+	private String deliveryType;
+	private String[] tagNames;
+	private MultipartFile[] files;
+	private Integer category;
+
+}
