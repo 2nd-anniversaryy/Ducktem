10a0b09dad9d59127a7cb7183484d0eb0734b007
jihyeon <biggums0223@gmail.com>
2023-01-31 18:10:27 +0900

    [#43] front post, delte Wish 구현

--- ducktem-app/src/components/wish.vue
+++ ducktem-app/src/components/wish.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <img v-if="wishStatus == 0" @click.prevent="checkClickHandler(id)" class="wish" src="/image/icon/heart.svg" alt="찜" />
-    <img v-if="wishStatus != 0" @click.prevent="unCheckClickHandler(id)" class="wish checked" src="/image/icon/icon-heart-red.svg" alt="찜" />
-
-    <div>{{ wishStatus }}</div>
+    <img v-if="myStatus == 0" @click.prevent="checkClickHandler(id)" class="wish" src="/image/icon/heart.svg" alt="찜" />
+    <img v-if="myStatus != 0" @click.prevent="unCheckClickHandler(id)" class="wish checked" src="/image/icon/icon-heart-red.svg" alt="찜" />
   </div>
 </template>
 
@@ -11,14 +9,46 @@
 export default {
   props: ['wishStatus', 'id'],
   data() {
-    return {};
+    return {
+      myStatus: this.$props.wishStatus,
+    };
   },
   methods: {
-    checkClickHandler(id) {
-      this.wishStatus = 0;
+    async checkClickHandler(productId) {
+      let myInfoForAuth;
+      if (this.$store.state.tokenResponse.access) {
+        myInfoForAuth = 'Bearer ' + this.$store.state.tokenResponse.access;
+      }
+      try {
+        const response = await fetch(`http://localhost:8080/wish/${productId}`, {
+          method: 'POST',
+          headers: {
+            Authorization: myInfoForAuth,
+          },
+        });
+      } catch (e) {
+        this.e = e;
+      } finally {
+        this.myStatus = 1;
+      }
     },
-    unCheckClickHandler(id) {
-      this.wishStatus = 1;
+    async unCheckClickHandler(productId) {
+      let myInfoForAuth;
+      if (this.$store.state.tokenResponse.access) {
+        myInfoForAuth = 'Bearer ' + this.$store.state.tokenResponse.access;
+      }
+      try {
+        const response = await fetch(`http://localhost:8080/wish/${productId}`, {
+          method: 'DELETE',
+          headers: {
+            Authorization: myInfoForAuth,
+          },
+        });
+      } catch (e) {
+        this.e = e;
+      } finally {
+        this.myStatus = 0;
+      }
     },
   },
 };
