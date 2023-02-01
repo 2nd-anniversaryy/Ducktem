3a4cb7cdf4b27fb2cdfb1dea4e64a5de3bcc04c5
miji7575 <mipark7575@gmail.com>
2023-02-02 01:17:28 +0900

    docs : tag 구현중 JPA Query 작성

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/repository/ProductTagRepository.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/repository/ProductTagRepository.java
@@ -1,8 +1,12 @@
 package com.ducktem.ducktemapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ducktem.ducktemapi.entity.ProductTag;
 
 public interface ProductTagRepository extends JpaRepository<ProductTag,Long> {
+
+
+	void deleteByProductId(Long id);
 }
