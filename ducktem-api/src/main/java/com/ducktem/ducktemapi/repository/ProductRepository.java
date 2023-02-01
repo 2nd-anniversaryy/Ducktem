fe0162c50fc5cd4eb23b6fa7c33c861e9061e85d
hantaehee <xogmlwkdi@naver.com>
2023-02-02 02:06:37 +0900

    fix : 상품 리스트 정렬 id 기준으로 desc 정렬

--- ducktem-api/src/main/java/com/ducktem/ducktemapi/repository/ProductRepository.java
+++ ducktem-api/src/main/java/com/ducktem/ducktemapi/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.ducktem.ducktemapi.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,13 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.ducktem.ducktemapi.dto.response.ProductPreviewResponse;
 import com.ducktem.ducktemapi.entity.Category;
-import com.ducktem.ducktemapi.entity.SuperCategory;
 import com.ducktem.ducktemapi.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-	Page<Product> findAll(Pageable pageable);
+	Page<Product> findAllByOrderByIdDesc(Pageable pageable);
 
 	@Query("SELECT count(p) from Product p where p.name like :query  and p.category in :category")
 	Long countProductByNameLikeQuery(@Param("query") String query, @Param("category") List<Category> category);
