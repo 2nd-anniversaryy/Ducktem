3d4caf5c17a48bee42fea9596a4eeac994f5265f
jihyeon <biggums0223@gmail.com>
2023-02-02 03:50:45 +0900

    회원탈퇴기능 프론트 1차 구현

--- ducktem-app/src/store.js
+++ ducktem-app/src/store.js
@@ -13,20 +13,20 @@ const state = {
   // junhyun
   userId: null,
   token: null,
-  tokenExpiration: null
+  tokenExpiration: null,
+  pwd: null,
 };
 const getters = {
   refresh: (state) => state.tokenResponse.refresh,
   access: (state) => state.tokenResponse.access,
   id: (state) => state.userId,
   nickname: (state) => state.nickName,
+  pwd: (state) => state.pwd,
 };
 const mutations = {
   // junhyun
   setUser(state, payload) {
-    state.token = payload.token,
-      state.userId = payload.userId,
-      state.tokenExpiration = payload.tokenExpiration
+    (state.token = payload.token), (state.userId = payload.userId), (state.tokenExpiration = payload.tokenExpiration);
   },
 
   login(state, item) {
@@ -40,6 +40,7 @@ const mutations = {
     state.tokenResponse.refresh = null;
     state.id = null;
     state.nickname = null;
+    state.pwd = null;
   },
 };
 const actions = {
@@ -55,15 +56,15 @@ const actions = {
         email: payload.email,
         phoneNumber: payload.phoneNumber,
         regDate: payload.regDate,
-        returnSecureToken: ture
-      })
+        returnSecureToken: ture,
+      }),
     });
 
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData)
-      const error = new Error(responseData.message || '인증에 실패하였습니다.')
+      console.log(responseData);
+      const error = new Error(responseData.message || '인증에 실패하였습니다.');
       throw error;
     }
 
@@ -71,8 +72,67 @@ const actions = {
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+
+  confirm({ commit }, { id, password }) {
+    let params = {
+      userId: id,
+      pwd: password,
+    };
+
+    const response = fetch(`http://localhost:8080/members`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json', //받는 형식
+        'Content-Type': 'application/json', //보내는 형식
+      },
+      body: JSON.stringify(params),
     })
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        if (result) {
+          commit('confirm', result);
+          if (params.pwd) {
+            state.pwd = params.pwd;
+          }
+          alert('인증되었습니다.');
+        } else {
+          console.log('로그인에 실패하였습니다.');
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        alert('로그인 서버 에러발생');
+      });
+  },
+  delete({ commit }, { id, password }) {
+    let params = {
+      userId: id,
+      pwd: password,
+    };
+
+    const response = fetch(`http://localhost:8080/members`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json', //받는 형식
+        'Content-Type': 'application/json', //보내는 형식
+      },
+      body: JSON.stringify(params),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then(() => {
+        alert('삭제되었습니다.');
+      })
+      .catch((e) => {
+        console.log(e);
+        alert('삭제에 실패하였습니다.');
+      });
   },
 
   login({ commit }, { id, password }) {
