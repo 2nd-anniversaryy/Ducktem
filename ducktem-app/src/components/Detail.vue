79a19f442aca90e17c643c6d6cd0d8a83c9508c9
leeminhae <official0333@gmail.com>
2023-02-02 11:30:25 +0900

    fix : 디테일 페이지 이미지 출력 오류 수정

--- ducktem-app/src/components/Detail.vue
+++ ducktem-app/src/components/Detail.vue
@@ -3,7 +3,7 @@
     <section class="main-wrap">
       <!-- 상품 이미지 -->
       <div class="product-img-wrap">
-        <img class="product-img" v-for="imgUrl in this.imgList" src="imgUrl" alt="" />
+        <img class="product-img" v-for="imgUrl in this.imgList" :src="imgUrl.imgUrl" alt="" />
       </div>
 
       <!-- 판매자 프로필 정보 -->
@@ -130,6 +130,7 @@ export default {
         this.e1 = e;
       } finally {
       }
+      console.log(this.imgList);
     },
     updateProduct() {
       this.$router.push({path: `/update-product/${this.$route.query.id}`,query:{id:`${this.$route.query.id}`}});
